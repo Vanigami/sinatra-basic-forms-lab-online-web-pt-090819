baf50690063655f919dcb45bba459028eb5e8989
Vanna Winland <vgwinland@gmail.com>
2019-12-17 22:33:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,4 +10,8 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
+  post '/display_puppy' do 
+    @puppy = Puppy.new(params['name'], params['breed'], params['months_old'])
+    erb :display_puppy
+  end
 end
